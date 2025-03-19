772f31ccfb090e25a92592aa40581085570ba66d
Otavio <estudosprogotavio@gmail.com>
2025-03-19 07:15:26 -0300

    Adicionando o Repositorio da entidade Produto

---
+++ src/main/java/dev/Zerphyis/monitoramento/Repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package dev.Zerphyis.monitoramento.Repository;
+
+import dev.Zerphyis.monitoramento.Entity.Product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
