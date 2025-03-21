488e404e17e55b06d52090fc8490ddca602dcbdc
Otavio <estudosprogotavio@gmail.com>
2025-03-21 19:24:38 -0300

    Adicionando a entidade  movimentaçãoEstoque

---
+++ src/main/java/dev/Zerphyis/monitoramento/Entity/MovimentStock/MovimentStock.java
@@ -0,0 +1,77 @@
+package dev.Zerphyis.monitoramento.Entity.MovimentStock;
+
+import dev.Zerphyis.monitoramento.Entity.Product.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "movimentacoes")
+public class MovimentStock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Product product;
+    private TypeMoviment typeMoviment;
+    @NotNull
+    private Integer amounts;
+    @NotNull
+    private LocalDate date;
+    @NotNull
+    private LocalTime time;
+
+    public MovimentStock() {
+
+    }
+
+    public MovimentStock(Product product, TypeMoviment typeMoviment, Integer amounts, LocalDate date, LocalTime time) {
+        this.product = product;
+        this.typeMoviment = typeMoviment;
+        this.amounts = amounts;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public TypeMoviment getTypeMoviment() {
+        return typeMoviment;
+    }
+
+    public void setTypeMoviment(TypeMoviment typeMoviment) {
+        this.typeMoviment = typeMoviment;
+    }
+
+    public Integer getAmounts() {
+        return amounts;
+    }
+
+    public void setAmounts(Integer amounts) {
+        this.amounts = amounts;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+}
