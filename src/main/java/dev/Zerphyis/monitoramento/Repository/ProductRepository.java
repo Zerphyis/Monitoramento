0c767700b51ce6610518254514759755243b04ee
Otavio <estudosprogotavio@gmail.com>
2025-03-21 19:25:49 -0300

    Adicionando a anotação de repositorios

--- src/main/java/dev/Zerphyis/monitoramento/Repository/ProductRepository.java
+++ src/main/java/dev/Zerphyis/monitoramento/Repository/ProductRepository.java
@@ -2,6 +2,8 @@ package dev.Zerphyis.monitoramento.Repository;
 
 import dev.Zerphyis.monitoramento.Entity.Product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
--- src/main/java/dev/Zerphyis/monitoramento/Repository/ProviderRepository.java
+++ src/main/java/dev/Zerphyis/monitoramento/Repository/ProviderRepository.java
@@ -2,6 +2,8 @@ package dev.Zerphyis.monitoramento.Repository;
 
 import dev.Zerphyis.monitoramento.Entity.Provider.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProviderRepository extends JpaRepository<Provider,Long> {
 }
