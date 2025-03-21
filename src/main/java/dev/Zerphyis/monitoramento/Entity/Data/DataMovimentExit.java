b09b640215fdb3a10a4458bb1459b7fcb4082284
Otavio <estudosprogotavio@gmail.com>
2025-03-21 19:27:19 -0300

    Record de formatação de saida da entiade MovimneetaçãoEstoque

---
+++ src/main/java/dev/Zerphyis/monitoramento/Entity/Data/DataMovimentExit.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.monitoramento.Entity.Data;
+
+import dev.Zerphyis.monitoramento.Entity.MovimentStock.TypeMoviment;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record DataMovimentExit(String Nameproduct,
+                               TypeMoviment typeMoviment,
+                               Integer amounts,
+                               LocalDate date,
+                               LocalTime time) {
+}
