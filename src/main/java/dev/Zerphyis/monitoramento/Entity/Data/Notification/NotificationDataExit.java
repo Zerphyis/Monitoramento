49ee51f112bca03e54d789f8b6e74aae21e79606
Otavio <estudosprogotavio@gmail.com>
2025-03-21 20:39:59 -0300

    Adicionando a record de formatação de saida da  entidade Notification

---
+++ src/main/java/dev/Zerphyis/monitoramento/Entity/Data/Notification/NotificationDataExit.java
@@ -0,0 +1,6 @@
+package dev.Zerphyis.monitoramento.Entity.Data.Notification;
+
+import java.time.LocalDateTime;
+
+public record NotificationDataExit(String nameMovimentProduct, String mensager, LocalDateTime dateNotification) {
+}
