bd8220f3f93e77a69629416983eee1db9aa638dd
Otavio <estudosprogotavio@gmail.com>
2025-03-22 09:28:59 -0300

    Atualizando record de entrada de Notification

--- src/main/java/dev/Zerphyis/monitoramento/Entity/Data/Notification/NotificationDataEntry.java
+++ src/main/java/dev/Zerphyis/monitoramento/Entity/Data/Notification/NotificationDataEntry.java
@@ -2,5 +2,5 @@ package dev.Zerphyis.monitoramento.Entity.Data.Notification;
 
 import java.time.LocalDateTime;
 
-public record NotificationDataEntry(Long movimentId, String mensager, LocalDateTime dateNotification) {
+public record NotificationDataEntry(Long movimentId, String mensage, LocalDateTime dateNotification) {
 }
