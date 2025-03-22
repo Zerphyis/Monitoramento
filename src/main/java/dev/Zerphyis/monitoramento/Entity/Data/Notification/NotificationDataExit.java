c867124d4c8ae1d9456881ee1a51551d4af2fa3d
Otavio <estudosprogotavio@gmail.com>
2025-03-22 09:28:52 -0300

    Atualizando record de saida de Notification

--- src/main/java/dev/Zerphyis/monitoramento/Entity/Data/Notification/NotificationDataExit.java
+++ src/main/java/dev/Zerphyis/monitoramento/Entity/Data/Notification/NotificationDataExit.java
@@ -2,5 +2,5 @@ package dev.Zerphyis.monitoramento.Entity.Data.Notification;
 
 import java.time.LocalDateTime;
 
-public record NotificationDataExit(String nameMovimentProduct, String mensager, LocalDateTime dateNotification) {
+public record NotificationDataExit(String productName, String mensage, LocalDateTime dateNotification) {
 }
