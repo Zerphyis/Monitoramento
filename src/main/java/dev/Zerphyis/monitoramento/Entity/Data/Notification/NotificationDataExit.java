31ab274be32ef3331c5b1580405e59e12eeace96
Otavio <estudosprogotavio@gmail.com>
2025-03-24 10:32:54 -0300

    Adicionando novo atributo no record de formatação

--- src/main/java/dev/Zerphyis/monitoramento/Entity/Data/Notification/NotificationDataExit.java
+++ src/main/java/dev/Zerphyis/monitoramento/Entity/Data/Notification/NotificationDataExit.java
@@ -2,5 +2,5 @@ package dev.Zerphyis.monitoramento.Entity.Data.Notification;
 
 import java.time.LocalDateTime;
 
-public record NotificationDataExit(String productName, String mensage, LocalDateTime dateNotification) {
+public record NotificationDataExit(Long movimentId, String productName, String mensage, LocalDateTime dateNotification) {
 }
